f4d25aeb2b4c4114ffdb1cf4fe4f336e450661e8
tonmoy <tonmoy3673@gmail.com>
2024-02-02 20:18:13 +0600

    display todos

--- src/component/Todos/Todo.jsx
+++ src/component/Todos/Todo.jsx
@@ -1,7 +1,18 @@
-const Todo = () => {
+import { Link } from "react-router-dom";
+
+const Todo = (todo) => {
+  const { id, title } = todo.todo;
   return (
     <div>
-      <h3>All Todos</h3>
+      <h3>ID : {id}</h3>
+      <h3>Title : {title}</h3>
+      <div className="py-2">
+        <Link to={`/todos/${id}`}>
+          <button className="bg-blue-500 p-2 rounded-md hover:bg-orange-600">
+            Status
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
--- src/component/Todos/Todos.jsx
+++ src/component/Todos/Todos.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData } from "react-router-dom";
+import Todo from "./Todo";
 
 const Todos = () => {
   const todos = useLoaderData();
@@ -6,6 +7,11 @@ const Todos = () => {
   return (
     <div className="text-center">
       <h3>Total Todos List : {todos.length}</h3>
+      <div className="grid md:grid-cols-4 grid-cols-1 px-12 py-12 gap-8">
+        {todos?.map((todo) => (
+          <Todo key={todo.id} todo={todo} />
+        ))}
+      </div>
     </div>
   );
 };
