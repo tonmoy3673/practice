ae5947bf74e136f5713f9a1183b1fb89d252ce84
tonmoy <tonmoy3673@gmail.com>
2024-02-02 17:04:30 +0600

    set navbar

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -2,12 +2,35 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      <ul>
-        <Link to="/">Home</Link>
-        <Link to="/posts">Posts</Link>
-        <Link to="/todos">Todos</Link>
-        <Link to="/users">Users</Link>
+    <div className="py-6 flex items-center px-10">
+      <h3 className="text-green-600 text-2xl hover:text-orange-600 cursor-pointer">
+        React Practice
+      </h3>
+      <ul className="mx-auto">
+        <Link
+          className="me-5 text-blue-600 hover:text-orange-600 cursor-pointer"
+          to="/"
+        >
+          Home
+        </Link>
+        <Link
+          className="me-5 text-blue-600 hover:text-orange-600 cursor-pointer"
+          to="/posts"
+        >
+          Posts
+        </Link>
+        <Link
+          className="me-5 text-blue-600 hover:text-orange-600 cursor-pointer"
+          to="/todos"
+        >
+          Todos
+        </Link>
+        <Link
+          className="me-5 text-blue-600 hover:text-orange-600 cursor-pointer"
+          to="/users"
+        >
+          Users
+        </Link>
       </ul>
     </div>
   );
